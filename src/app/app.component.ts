f97328789d2074e6a9270a33bbed6b8e59d6b50a
Pankaj Sharma <dear.pankaj29@gmail.com>
2019-12-28 23:44:40 +0530

    initial commit for reactive form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'reactive-forms';
+export class AppComponent implements OnInit {
+  title = 'Reactive Forms';
+  submitted = false;
+  reactiveForm: FormGroup;
+  gender = ['Male', 'Female'];
+  forbiddenUserNames = ['Test1', 'Test2'];
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      empData: new FormGroup({
+        fullName: new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email])
+      }),
+      gender: new FormControl('Male'),
+      SecurityQuestion: new FormControl('schoolName'),
+      questionAnswer: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    if (this.reactiveForm.status === 'VALID') {
+      this.submitted = true;
+      this.reactiveForm.reset({
+        empData: {
+          fullName: '',
+          email: ''
+        },
+        gender: 'Male',
+        SecurityQuestion: 'schoolName',
+        questionAnswer: ''
+      });
+    }
+  }
+
+  // Custom validators
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+        return {nameIsForbidden: true};
+    }
+    return null;
+  }
 }
